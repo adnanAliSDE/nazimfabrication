568731fbd669b1aed8db4170c00fbd4df1f99866
Adnan Ali <112294571+adnanAliSDE@users.noreply.github.com>
2025-11-08 16:10:00 +0530

    Populated the current year in copyright dynamically.

--- static/scripts/global.js
+++ static/scripts/global.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const burgerMenu = document.getElementById('burger-menu');
     const overlayMenu = document.getElementById('overlay-menu');
     const closeButton = document.getElementById('close-menu');
+    const yearContainer = document.querySelector('span.year-container');
+    const currentYear = new Date().getFullYear();
+
+    yearContainer.innerHTML = `${currentYear}-${String(currentYear + 1).slice(-2,)};
 
     burgerMenu.addEventListener('click', function () {
         overlayMenu.classList.toggle('hidden');
